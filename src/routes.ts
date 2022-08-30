75b37b401c9cfeae1ccda517198d6772c46d0f87
Pablo Ykaro <pabloykaro9@gmailcom>
2022-08-30 14:01:24 -0300

    new route update

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import { Router, Request, Response } from 'express';
 
 import ControllersCreateClient from './controllers/controllersCreateClient';
+import ControllersCreateBank from './controllers/controllersCreateBank';
 
 const routes = Router();
 
 const useControllersCreateClient = new ControllersCreateClient;
-routes.post('/createclient', (req: Request, res: Response) => useControllersCreateClient.loadCreate(req,res));
+routes.post('/createclient', (req: Request, res: Response) => useControllersCreateClient.handle(req,res));
+
+const useControllersCreateBank = new ControllersCreateBank;
+routes.post('/createbank', (req: Request, res: Response) => useControllersCreateBank.handle(req,res));
 
 export default routes;
