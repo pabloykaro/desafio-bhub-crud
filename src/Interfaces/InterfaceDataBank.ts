8f915682d8734701f3c790e8672f7f13fc05708c
Pablo Ykaro <pabloykaro9@gmailcom>
2022-09-06 08:56:47 -0300

    interace types

---
+++ src/Interfaces/InterfaceDataBank.ts
@@ -0,0 +1,9 @@
+interface InteraceDataBank{
+  id_bank: number,
+  id_pk_client: number,
+  agency_account: string,
+  account_bank: string,
+  bank_name: string,
+}
+
+export { InteraceDataBank };
---
+++ src/Interfaces/InterfaceDataClient.ts
@@ -0,0 +1,12 @@
+interface InterfaceDataClient{
+  id_client: number,
+  corporate_name: string,
+  cnpj_number: string,
+  telephone_number: string,
+  address_city: string,
+  date_register_account: Date,
+  billing_declared:string
+  quantityBank: number,
+}
+
+export { InterfaceDataClient };
