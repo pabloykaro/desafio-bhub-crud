1651b256448a1b71118a91dd48cafc2e246e3e40
Pablo Ykaro <pabloykaro9@gmailcom>
2022-09-06 09:54:28 -0300

    added all

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import { useVarEnvs } from './middlewares/useVarEnvs';
 import routes from './routes';
 
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 5050;
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 
