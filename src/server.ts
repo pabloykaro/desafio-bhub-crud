c7d24bc44d897c8f85c26fc765bc42ad71077d68
Pablo Ykaro <pabloykaro9@gmailcom>
2022-09-06 08:58:00 -0300

    added new func server

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,15 @@
 import express from 'express';
+import { useVarEnvs } from './middlewares/useVarEnvs';
 import routes from './routes';
+
+useVarEnvs();
+
+const PORT = 3333;
+const HOST = '0.0.0.0';
 
 const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => 'server is running');
+
+app.listen(PORT,HOST);
